b81ed4869bee7c9b4926ec31e3a7a0419a761498
Ewerton Campos <ewtn@live.com>
2018-04-21 19:58:25 -0300

    Projeto web angularJS

---
+++ src/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('angularAPP', ['ngRoute', 'ngSanitize', 'ui.bootstrap'])
+
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl:'spas/consultar/consultar-tpl.html'
+        })
+        .otherwise({
+            redirectTo:'/'
+        });
+}]);
---
+++ src/spas/consultar/consultar-controllers.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('angularAPP')
+
+.controller('consultarController', consultarController);
+
+function consultarController()
+{
+    var vm = this;
+
+    vm.teste = 'testando';
+}
